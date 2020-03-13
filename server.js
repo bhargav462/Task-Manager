cf8a88462ff3111a87172368818bd6d4d1bd151e
Nakkina Bhargav <43701429+bhargav462@users.noreply.github.com>
2020-03-14 02:17:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',(req,res) => {
 //   res.sendFile('chessPhotos.html',{root:__dirname + '/public'});
+console.log('get',__dirname);
      res.sendFile('register.html',{root:__dirname + '/public'})
 });
   
